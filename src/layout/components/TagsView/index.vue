afb09ff11e67a5637763c94ed8d636500481276b
Edwin Nie <cookienfy@hotmail.com>
2020-07-07 17:40:51 +0800

    Update

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -225,9 +225,9 @@ export default {
         margin-right: 15px;
       }
       &.active {
-        background-color: #42b983;
+        background-color: #04246A;
         color: #fff;
-        border-color: #42b983;
+        border-color: #04246A;
         &::before {
           content: '';
           background: #fff;
